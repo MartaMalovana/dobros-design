043a43c71383ba0ffe8ce9d4038d0a37086e51f5
MartaMalovana <marta.malovana@gmail.com>
2022-11-23 19:56:32 +0200

    delete bugs

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,7 +1,7 @@
 import { Content } from "../../App.styled";
-import {Container, HeroBox, Photo, Title, Text, Text1, Burger, Logo} from './Hero.styled';
+import {Container, HeroBox, Photo, Title, Text, Text1, Burger} from './Hero.styled';
 import photo1 from '../../images/jpg/hero.jpg';
-import logo from '../../images/jpg/logo.jpg';
+// import logo from '../../images/jpg/logo.jpg';
 
 export default function Hero () {
     return (
